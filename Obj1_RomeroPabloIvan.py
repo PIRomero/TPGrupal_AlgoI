d317f58139a57491e618186eb0ce81d9cf656c52
Pablo <Pablo.Romero@Sarsy-srl.com>
2025-11-10 00:46:44 -0300

    Objetivo 2

--- Obj1_RomeroPabloIvan.py
+++ Obj1_RomeroPabloIvan.py
@@ -1,7 +1,7 @@
 import doctest
 
-def cifrado_cesar(palabra, clave):
-    #Cifra la palabra desplazando cada caracter la cantidad de espacios indicados por la clave
+def cifrado_cesar(mensaje, clave):
+    #Cifra el mensaje mensaje desplazando cada caracter la cantidad de espacios indicados por la clave en el alfabeto
     """
     >>> cifrado_cesar("hola", 1)
     'ipmb'
@@ -25,17 +25,19 @@ def cifrado_cesar(palabra, clave):
     'CcBb'
     """
 
+    CANTIDAD_LETRAS = 26
+    CANTIDAD_NUMEROS = 10
     resultado = []
-
-    for caracter in palabra:
+    
+    for caracter in mensaje:
         if 'a' <= caracter <= 'z':
-            nuevo = chr((ord(caracter) - ord('a') + clave) % 26 + ord('a'))
+            nuevo = chr((ord(caracter) - ord('a') + clave) % CANTIDAD_LETRAS + ord('a'))
             resultado.append(nuevo)
         elif 'A' <= caracter <= 'Z':
-            nuevo = chr((ord(caracter) - ord('A') + clave) % 26 + ord('A'))
+            nuevo = chr((ord(caracter) - ord('A') + clave) % CANTIDAD_LETRAS + ord('A'))
             resultado.append(nuevo)
         elif  '0' <= caracter <= '9':
-            nuevo = chr((ord(caracter) - ord('0') + clave) % 10 + ord('0'))
+            nuevo = chr((ord(caracter) - ord('0') + clave) % CANTIDAD_NUMEROS + ord('0'))
             resultado.append(nuevo)
         else:
             resultado.append(caracter)
---
+++ Obj2_RomeroPabloIvan.py
@@ -0,0 +1,47 @@
+import doctest
+
+def cifrado_atbash(mensaje):
+    #Cifra el mensaje mensaje asignandole a cada caracter el valor de la letra ubicada en el lado contrario del abecedario y cambiando 
+    """
+    >>> cifrado_atbash("hola")
+    'SLOZ'
+    >>> cifrado_atbash("HOLA")
+    'sloz'
+    >>> cifrado_atbash("xyz")
+    'CBA'
+    >>> cifrado_atbash("XYZ")
+    'cba'
+    >>> cifrado_atbash("Año 2025")
+    'zML 7974'
+    >>> cifrado_atbash("ABC")
+    'zyx'
+    >>> cifrado_atbash("abc")
+    'ZYX'
+    >>> cifrado_atbash("Python 3.9.0")
+    'kBGSLN 6.0.9'
+    >>> cifrado_atbash("Cifrado Atbash")
+    'xRUIZWL zGYZHS'
+    >>> cifrado_atbash("AaZz")
+    'zZaA'
+    """
+    minusculas = "abcdefghijklmnñopqrstuvwxyz"
+    mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+    resultado = []
+
+    for caracter in mensaje:
+        if caracter in minusculas:
+            nuevo = mayusculas[len(minusculas) - 1 - minusculas.index(caracter)]
+            resultado.append(nuevo)
+        elif caracter in mayusculas:
+            nuevo = minusculas[len(mayusculas) - 1 - mayusculas.index(caracter)]
+            resultado.append(nuevo)
+        elif '0' <= caracter <= '9':
+            nuevo = chr(ord('9') - (ord(caracter) - ord('0')))
+            resultado.append(nuevo)
+        else:
+            resultado.append(caracter)
+
+    return "".join(resultado)
+
+print(doctest.testmod())
